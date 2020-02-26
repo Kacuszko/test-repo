c834e5e4a75260a472c27fd95492471cf3e099d1
Kacuszko <kacuszko@gmail.com>
2020-02-26 18:29:53 +0100

    Added sudoku_resolver to solve sudoku board. Classes.py expanded by one more subclass and magic method

--- classes.py
+++ classes.py
@@ -22,7 +22,46 @@ class Employee:
         self.pay = int(self.pay * self.raise_amount)
 
 
-emp_1 = Employee('Corey', 'Schafer', 500000)
-emp_2 = Employee('Mateusz', 'Kac', 600000)
+class Developer(Employee):
 
-print(Employee.num_of_emps)
+    def __init__(self, first, last, pay, position):
+        super().__init__(first, last, pay)
+        self.position = position
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def del_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def show_emp(self):
+        for emp in self.employees:
+            if hasattr(emp, 'position'):
+                print('--> ' + emp.fullname() + ' working as ' + emp.position)
+            else:
+                print('--> ' + emp.fullname())
+
+    def __str__(self):
+        return ('{} {}'.format(self.first, self.last) +
+                ' has salary {}'.format(self.pay))
+
+
+emp_1 = Employee('Mateusz', 'Kac', 20000)
+dev_1 = Developer('Marcin', 'Jackowicz', 10000, 'Java')
+mgr_1 = Manager('Mariusz', 'Jakubiak', 20000)
+mgr_1.add_emp(emp_1)
+mgr_1.add_emp(dev_1)
+mgr_1.show_emp()
+print(str(mgr_1))
